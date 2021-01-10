7abab45942c11174bae290cc3eb210f6aa6c35c0
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2021-01-09 22:13:48 -0300

    Add ProjectDAO interface methods

--- src/main/project/java/dao/ProjectDAO.java
+++ src/main/project/java/dao/ProjectDAO.java
@@ -1,5 +1,23 @@
 package main.project.java.dao;
 
-public interface ProjectDAO {
+import java.util.List;
 
+import main.project.java.model.Processos;
+
+
+public interface ProjectDAO {
+	
+	public void saveOrUpdate(Processos project);
+	public void saveOrUpdateP(Processos project);
+	
+	public int delete(int projectId);
+	public int deleteP(int projectId);
+    
+    public Processos get(int projectId);
+    public Processos getProcesso(String numeroP);
+    public Processos getP(int projectId);
+     
+    public List<Processos> list();
+    public List<Processos> listP();
+    public List<Processos> listTP();
 }
