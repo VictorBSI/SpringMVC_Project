4bdae46e0a1c376021781f2971b40cda2c50f716
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2021-01-09 21:58:16 -0300

    Add Database and ViewResolver configurations

--- src/main/project/java/config/MvcConfiguration.java
+++ src/main/project/java/config/MvcConfiguration.java
@@ -1,5 +1,51 @@
 package main.project.java.config;
 
-public class MvcConfiguration {
+import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import main.project.java.dao.ProjectDAO;
+import main.project.java.dao.ProjectDAOImpl;
+
+@Configuration
+@ComponentScan(basePackages = "main.project.java")
+@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	//configurar banco de dados
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/teste1db?useTimezone=true&serverTimezone=UTC");
+		dataSource.setUsername("root");
+		dataSource.setPassword("R@@t");
+
+		return dataSource;
+	}
+
+	@Bean
+	public ProjectDAO getProjectDAO() {
+		return new ProjectDAOImpl(getDataSource());
+	}
 }
